788fb83541838b87e1fa3e26e2252eee276cbbab
Huda Abdela <hudaabdulah75@gmail.com>
2024-11-22 20:25:58 +0300

    u

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,8 +18,7 @@ urlpatterns = [
     path('member-view/', views.member_view, name='member_view'),
 
     # Secured book views
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/add/', views.add_book, name='add_book'),  # Adding a book
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),  # Editing a book
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),  # Deleting a book
 ]
-
