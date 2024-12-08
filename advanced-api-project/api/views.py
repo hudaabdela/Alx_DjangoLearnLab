4d1cce55f33f4cffd14b0109b5c3173354a1221d
Huda Abdela <hudaabdulah75@gmail.com>
2024-12-08 10:22:55 +0300

    d

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly  
 from .serializers import BookSerializer
 
 
