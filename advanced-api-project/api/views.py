a3c3fd28f2e0342d09217e3aff9f88543b133517
Huda Abdela <hudaabdulah75@gmail.com>
2024-12-08 10:29:03 +0300

    authenticate2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework import generics, permissions, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import generics, status
 from rest_framework.response import Response
+from .models import Book
 from .serializers import BookSerializer
 
 
@@ -25,11 +26,10 @@ class CreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]  
 
     def create(self, request, *args, **kwargs):
-        """Custom create logic with enhanced response."""
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        
+
         return Response(
             {"message": "Book created successfully!", "data": serializer.data},
             status=status.HTTP_201_CREATED
@@ -43,13 +43,12 @@ class UpdateView(generics.UpdateAPIView):
     permission_classes = [IsAuthenticated]  
 
     def update(self, request, *args, **kwargs):
-        """Custom update logic with enhanced response."""
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-        
+
         return Response(
             {"message": "Book updated successfully!", "data": serializer.data},
             status=status.HTTP_200_OK
@@ -62,7 +61,6 @@ class DeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]  
 
     def delete(self, request, *args, **kwargs):
-        """Custom delete response message."""
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(
